3427187c1d17507c792a8553f195c2fa67264c5e
DanielcmSantana <81927509+DanielcmSantana@users.noreply.github.com>
2021-07-21 10:52:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var valorEmDolarTexto = prompt("Valor em dolar")
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+
+var valorEmReal = valorEmDolarNumero * 5.7
+var valorEmRealFixado = valorEmReal.toFixed(2)
+alert (valorEmRealFixado)
+
+console.log(valorEmRealFixado)
+
